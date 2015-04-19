5a0f7ce3809ce6fe97602cefe40c8c3ab03b45c9
Javier Carrascal <javilumbrales@gmail.com>
2015-04-19 16:12:26 +0100

    Allow to have different missions per language, filterable by the api

--- appengine/whatdoyoudo/admin.py
+++ appengine/whatdoyoudo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from whatdoyoudo.models import Mission, MissionNode
 
 class MissionAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "beginning", "created")
+    list_display = ("id", "name", "beginning", "language", "created")
     #ordering = ('-created',)
 class MissionNodeAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "question", "status", "mission", "node_1", "node_2", "created")
