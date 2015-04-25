ec72943d2ed1f894edc16b7fd3c9df2291e8d98d
Javier Carrascal <javilumbrales@gmail.com>
2015-04-25 19:03:55 +0100

    Add clone mission feature

--- appengine/whatdoyoudo/admin.py
+++ appengine/whatdoyoudo/admin.py
@@ -6,14 +6,41 @@ import copy  # (1) use python copy
 
 def clone_mission(modeladmin, request, queryset):
     # sd is an instance of SemesterDetails
-    for sd in queryset:
-        print sd
-        sd_copy = copy.copy(sd) # (2) django copy object
-        sd_copy.id = None   # (3) set 'id' to None to create new object
-        sd_copy.created = timezone.now()
-        sd_copy.active = False
-        sd_copy.name = sd_copy.name + " (copy)"
-        sd_copy.save()    # initial save
+    for mission in queryset:
+        #print mission
+        mission_copy = copy.copy(mission) # (2) django copy object
+        mission_copy.id = None   # (3) set 'id' to None to create new object
+        mission_copy.created = timezone.now()
+        mission_copy.active = False
+        mission_copy.name = mission_copy.name + " (copy)"
+        mission_copy.save()    # initial save
+
+        #clone all nodes
+        nodes = MissionNode.objects.filter(mission=mission)
+        clonedNodes = {}
+        for node in nodes:
+            #print node
+            node_copy = copy.copy(node) # (2) django copy object
+            node_copy.id = None   # (3) set 'id' to None to create new object
+            node_copy.mission = mission_copy
+            node_copy.created = timezone.now()
+            node_copy.name = node.name + " (copy)"
+            node_copy.save()    # initial save
+            clonedNodes[node] = node_copy
+
+        #print clonedNodes
+        #print mission_copy.id
+        #reasign references
+        for oldNode in clonedNodes:
+            newNode = clonedNodes[oldNode]
+            #print 'cloned ' + str(newNode)
+            if newNode.node_1 is not None:
+                newNode.node_1 = clonedNodes[newNode.node_1]
+                newNode.save()
+
+            if newNode.node_2 is not None:
+                newNode.node_2 = clonedNodes[newNode.node_2]
+                newNode.save()
     return True
 
 class MissionAdmin(admin.ModelAdmin):
